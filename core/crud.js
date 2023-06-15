3377deb614a3e8aa77c5c182785e0cc3a356a3f6
jeferson-franco <jefersonfranco@pm.me>
2023-06-15 08:44:07 -0300

    update 1 file and create 1 file DB_FILE_PATH

--- core/crud.js
+++ core/crud.js
@@ -1,8 +1,13 @@
+const fs = require("fs");
+const DB_FILE_PATH = "./core/db";
+
 console.log("[CRUD]");
 
 function create(content) {
+    // salvar o content no sistema
+    fs.writeFileSync(DB_FILE_PATH, content);
     return content;
 }
 
 // [SIMULATION]
-create("Hello World");
+console.log(create("Hello DB World"));
